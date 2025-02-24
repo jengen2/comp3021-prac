dae92edb6d7d66e47d91b0c41878dbd647b98b20
Justin Engen <155939991+jengen2@users.noreply.github.com>
2025-02-24 09:54:39 -0600

    Update sample.py

--- sample.py
+++ sample.py
@@ -10,7 +10,7 @@ if name == "main":
 numbers = [1, 2, 3, 4, 5, 6, 7, 8]
 
 for num in numbers:
-    print num
+    print(num)
 
 dict_example = {"key1": "value1", "key2": "value2"}
 print(dict_example["key1"])
