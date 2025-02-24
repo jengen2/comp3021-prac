7708a4d4df90cf7da33e50769db65b9de92c801c
Justin Engen <155939991+jengen2@users.noreply.github.com>
2025-02-24 09:30:52 -0600

    Create sample.py

---
+++ sample.py
@@ -0,0 +1,17 @@
+def greet(name)
+    print("Hello, " + name)
+
+def add_numbers(a, b)
+    return a + b
+
+if name == "main":
+print("This should be indented")
+
+numbers = [1, 2, 3, 4,
+5, 6, 7, 8]
+
+for num in numbers
+    print num
+
+dict_example = { "key1": "value1", "key2": "value2"
+print(dict_example["key1"])
